1e52d761b4d4d656cb49e3a066233032556c7c64
Francisco Diez <fran19842010@live.com>
2024-02-27 20:56:33 +0100

    css added

---
+++ assignClients.js
@@ -0,0 +1,37 @@
+
+  function assignClients(trainers, clients) {
+
+    trainers.sort((a, b) => b.reputation - a.reputation);                                                   //ordeno de mayor a menor la reputacion de entrenadores
+
+    clients.sort((a, b) => b.reputationImportance - a.reputationImportance);                                //ordeno de mayor a menor la importancia de reputación del cliente
+  
+    const assignment = {};
+  
+    clients.forEach(client => {
+      let bestTrainer = null;
+      let maxSatisfaction = -1;
+  
+      trainers.forEach(trainer => {
+        if (trainer.availableSlots > 0) {
+          let satisfaction = 0;                                                                             //satisfacción potencial del cliente
+          satisfaction = client.reputationImportance * trainer.reputation;
+
+          if (satisfaction > maxSatisfaction) {                                                             //actualizar si hay mayor satisfacción
+            maxSatisfaction = satisfaction;
+            bestTrainer = trainer;
+          }
+        }
+      });
+
+      if (bestTrainer) {                                                                                    //asignar al mejor entrenador encontrado/posible
+        if (!assignment[bestTrainer.name]) {
+          assignment[bestTrainer.name] = [];
+        }
+        assignment[bestTrainer.name].push(client.name);
+        bestTrainer.availableSlots--;
+      }
+    });
+  
+    //console.log(assignment);
+    return assignment;
+  } 
